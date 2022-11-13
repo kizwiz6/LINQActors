50ba96e3e93646b43bf86a7796d807a941894fa7
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-13 17:22:26 +0000

    Add project files.

---
+++ Actor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQActors
+{
+    public class Actor
+    {
+        public string Name { get; set; }
+        public decimal TotalGross { get; set; }
+        public int MovieCount { get; set; }
+        public decimal AveragePerMovie { get; set; }
+        public string TopMovie { get; set; }
+        public decimal TopMovieGross { get; set; }
+
+    }
+}
